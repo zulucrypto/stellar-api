39e7448d8f8d8d04e23538fab22c628b892fd9f7
ZuluCrypto <zulucrypto@protonmail.com>
2017-12-20 17:51:15 -0700

    Implemented XDR Price

---
+++ src/XdrModel/Price.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace ZuluCrypto\StellarSdk\XdrModel;
+
+
+use ZuluCrypto\StellarSdk\Xdr\Iface\XdrEncodableInterface;
+use ZuluCrypto\StellarSdk\Xdr\XdrEncoder;
+
+/**
+ * https://github.com/stellar/stellar-core/blob/master/src/xdr/Stellar-ledger-entries.x#L47
+ */
+class Price implements XdrEncodableInterface
+{
+    /**
+     * @var int
+     */
+    protected $numerator;
+
+    /**
+     * @var int
+     */
+    protected $denominator;
+
+    public function __construct($numerator, $denominator)
+    {
+        $this->numerator = $numerator;
+        $this->denominator = $denominator;
+    }
+
+    public function toXdr()
+    {
+        $bytes = '';
+
+        $bytes .= XdrEncoder::unsignedInteger($this->numerator);
+        $bytes .= XdrEncoder::unsignedInteger($this->denominator);
+
+        return $bytes;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumerator()
+    {
+        return $this->numerator;
+    }
+
+    /**
+     * @param int $numerator
+     */
+    public function setNumerator($numerator)
+    {
+        $this->numerator = $numerator;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDenominator()
+    {
+        return $this->denominator;
+    }
+
+    /**
+     * @param int $denominator
+     */
+    public function setDenominator($denominator)
+    {
+        $this->denominator = $denominator;
+    }
+}
