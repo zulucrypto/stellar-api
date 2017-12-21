fd8ea863fe00dc422f57596667b5eaa0899a0927
ZuluCrypto <zulucrypto@protonmail.com>
2017-12-20 17:48:35 -0700

    run-integration.sh now uses the correct group and supports passing additional arguments

--- tests/run-integration.sh
+++ tests/run-integration.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#
+# Usage Examples:
+#
+#  ./run-integration.sh --filter testCreateAccount
+#
 
 # Directory where this script is located
 DIR="$( cd "$( dirname "$0" )" && pwd )"
@@ -6,4 +11,4 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 # Run relative to the tests/ directory
 cd "$DIR"
 
-../vendor/bin/phpunit -c "$DIR" --group integration
+../vendor/bin/phpunit -c "$DIR" --group requires-integrationnet "$@"
