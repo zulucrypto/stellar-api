c79867829c32af54f45da846d9e656581907dc4b
ZuluCrypto <zulucrypto@protonmail.com>
2017-12-20 17:50:55 -0700

    XDR Memo now supports all memo types

--- src/XdrModel/Memo.php
+++ src/XdrModel/Memo.php
@@ -3,6 +3,7 @@
 namespace ZuluCrypto\StellarSdk\XdrModel;
 
 
+use ZuluCrypto\StellarSdk\Util\Debug;
 use ZuluCrypto\StellarSdk\Xdr\XdrEncoder;
 
 /**
@@ -26,6 +27,9 @@ class Memo
     const MEMO_TYPE_HASH = 3;
     const MEMO_TYPE_RETURN = 4;
 
+    // Text memos can be up to 28 characters
+    const VALUE_TEXT_MAX_SIZE = 28;
+
     /**
      * See the MEMO_TYPE constants
      *
@@ -44,17 +48,52 @@ class Memo
     {
         $this->type = $type;
         $this->value = $value;
+
+        $this->validate();
+    }
+
+    public function validate()
+    {
+        if ($this->type == static::MEMO_TYPE_NONE) return;
+        if ($this->type == static::MEMO_TYPE_TEXT) {
+            // Verify length does not exceed max
+            if (strlen($this->value) > static::VALUE_TEXT_MAX_SIZE) {
+                throw new \ErrorException(sprintf('memo text is greater than the maximum of %s bytes', static::VALUE_TEXT_MAX_SIZE));
+            }
+        }
+        if ($this->type == static::MEMO_TYPE_ID) {
+            if ($this->value < 0) throw new \ErrorException('value cannot be negative');
+            if ($this->value > PHP_INT_MAX) throw new \ErrorException(sprintf('value cannot be larger than %s', PHP_INT_MAX));
+        }
+        if ($this->type == static::MEMO_TYPE_HASH || $this->type == static::MEMO_TYPE_RETURN) {
+            if (strlen($this->value) != 32) throw new \InvalidArgumentException(sprintf('hash values must be 32 bytes, got %s bytes', strlen($this->value)));
+        }
     }
 
     public function toXdr()
     {
+        $this->validate();
         $bytes = '';
 
         // Type
-        $bytes .= XdrEncoder::unsignedInteger(self::MEMO_TYPE_NONE);
+        $bytes .= XdrEncoder::unsignedInteger($this->type);
 
         // Value
-        // todo
+        if ($this->type == static::MEMO_TYPE_NONE) {
+            // no-op
+        }
+        if ($this->type == static::MEMO_TYPE_TEXT) {
+            $bytes .= XdrEncoder::string($this->value, static::VALUE_TEXT_MAX_SIZE);
+        }
+        if ($this->type == static::MEMO_TYPE_ID) {
+            $bytes .= XdrEncoder::unsignedInteger64($this->value);
+        }
+        if ($this->type == static::MEMO_TYPE_HASH) {
+            $bytes .= XdrEncoder::opaqueFixed($this->value, 32);
+        }
+        if ($this->type == static::MEMO_TYPE_RETURN) {
+            $bytes .= XdrEncoder::opaqueFixed($this->value, 32);
+        }
 
         return $bytes;
     }
