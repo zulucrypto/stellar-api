494f88937da7563d213cd1c097eccee85766489e
ZuluCrypto <zulucrypto@protonmail.com>
2018-03-03 00:54:12 -0700

    finished TransactionResultTest

--- tests/Unit/XdrModel/TransactionResultTest.php
+++ tests/Unit/XdrModel/TransactionResultTest.php
@@ -6,6 +6,7 @@ namespace ZuluCrypto\StellarSdk\Test\Unit\XdrModel;
 
 use PHPUnit\Framework\TestCase;
 use ZuluCrypto\StellarSdk\Xdr\XdrBuffer;
+use ZuluCrypto\StellarSdk\XdrModel\PaymentResult;
 use ZuluCrypto\StellarSdk\XdrModel\TransactionResult;
 
 class TransactionResultTest extends TestCase
@@ -16,5 +17,16 @@ class TransactionResultTest extends TestCase
         $xdr = new XdrBuffer(base64_decode('AAAAAAAAAGQAAAAAAAAAAQAAAAAAAAABAAAAAAAAAAA='));
 
         $result = TransactionResult::fromXdr($xdr);
+
+        $opResults = $result->getOperationResults();
+
+        $this->assertEquals(TransactionResult::SUCCESS, $result->getResultCode());
+        $this->assertCount(1, $opResults);
+        $this->assertEquals(100, $result->getFeeCharged()->getUnscaledString());
+        $this->assertTrue($result->succeeded());
+
+        $paymentResult = $opResults[0];
+        $this->assertTrue($paymentResult instanceof PaymentResult);
+        $this->assertTrue($paymentResult->succeeded());
     }
 }
