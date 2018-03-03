05f0d43b923b244c9b1cea12650c9fc7156551c6
ZuluCrypto <zulucrypto@protonmail.com>
2018-03-03 00:54:32 -0700

    committing TransactionBuidlerTest::testGetXdr to help with future XDR tests

--- tests/integration/TransactionBuilderTest.php
+++ tests/integration/TransactionBuilderTest.php
@@ -5,7 +5,15 @@ namespace ZuluCrypto\StellarSdk\Test\Integration;
 
 
 use ZuluCrypto\StellarSdk\Horizon\Exception\PostTransactionException;
+use ZuluCrypto\StellarSdk\Keypair;
 use ZuluCrypto\StellarSdk\Test\Util\IntegrationTest;
+use ZuluCrypto\StellarSdk\XdrModel\Asset;
+use ZuluCrypto\StellarSdk\XdrModel\Operation\InflationOp;
+use ZuluCrypto\StellarSdk\XdrModel\Operation\ManageDataOp;
+use ZuluCrypto\StellarSdk\XdrModel\Operation\ManageOfferOp;
+use ZuluCrypto\StellarSdk\XdrModel\Operation\PathPaymentOp;
+use ZuluCrypto\StellarSdk\XdrModel\Operation\SetOptionsOp;
+use ZuluCrypto\StellarSdk\XdrModel\Price;
 
 class TransactionBuilderTest extends IntegrationTest
 {
@@ -26,6 +34,41 @@ class TransactionBuilderTest extends IntegrationTest
 
         $this->assertTrue($result->succeeded());
         $this->assertCount(1, $result->getOperationResults());
+    }
+
+    /**
+     * Helper method to extract OperationResult XDR for writing other tests / debugging
+     *
+     * @group requires-integrationnet
+     */
+    public function testGetXdr()
+    {
+        $this->markTestSkipped('For debugging');
+
+        $sourceKeypair = $this->fixtureAccounts['basic1'];
+        $destinationKeypair = $this->fixtureAccounts['basic2'];
+        $usdIssuingKeypair = Keypair::newFromSeed('SBJXZEVYRX244HKDY6L5JZYPWDQW6D3WLEE3PTMQM4CSUKGE37J4AC3W');
+        $usdBankKeypair = Keypair::newFromSeed('SDJOXTS4TE3Q3HUIFQK5AQCTRML6HIOUQIXDLCEQHICOFHU5CQN6DBLS');
+        $authRequiredIssuingKeypair = Keypair::newFromSeed('SABFYGWPSP3EEJ2EURHQYAIRTNK3SVQPED5PWOHGCWKPZBSCWBV4QGKE');
+
+        $usdAsset = Asset::newCustomAsset('USDTEST', $usdIssuingKeypair->getPublicKey());
+        $authRequiredAsset = Asset::newCustomAsset('AUTHREQ', $authRequiredIssuingKeypair->getPublicKey());
+
+        //$op = new ManageOfferOp($usdAsset, Asset::newNativeAsset(), 0, new Price(3, 1), 8);
+
+        $op = new ManageDataOp('asdf', 'jkl');
+
+        $response = $this->horizonServer->buildTransaction($sourceKeypair)
+            ->addOperation($op)
+            ->submit($sourceKeypair);
+
+        $rawData = $response->getRawData();
+        $xdrB64 = $rawData['result_xdr'];
+        $xdr = base64_decode($xdrB64);
+
+        $xdr = substr($xdr,8 + 4 + 4);
+        print "XDR: \n";
+        print base64_encode($xdr);
     }
 
     /**
